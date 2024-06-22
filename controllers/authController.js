bbbd5225be2f7c7747628e41f598a5f5c2c9d525
SANJAY <sanjaypk986@gmail.com>
2024-06-22 11:51:44 +0530

    Created Jwt Token and created secret key

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,29 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
-const login = async(req, res) => {
-    // get data
-    const data = req.body
-    // compare email with database using find one
-    const user = await User.findOne({ email: data.email }).exec();
-    // user not available
-    if (!user) {
-        return res.status(401).send("invalid Email id")
-    }
-    // check password with database with bcrypt compare
-    const passwordMatch = bcrypt.compareSync(data.password, user.password);
-    if (passwordMatch) {
-        res.send("login")
-    }else{
-        res.status(401).send("Unauthoraized Access! Wrong Password")
-      }
-}
+const login = async (req, res) => {
+  // get data
+  const data = req.body;
+  // compare email with database using find one
+  const user = await User.findOne({ email: data.email }).exec();
+  // user not available
+  if (!user) {
+    return res.status(401).send("invalid Email id");
+  }
+  // check password with database with bcrypt compare
+  const passwordMatch = bcrypt.compareSync(data.password, user.password);
+  if (passwordMatch) {
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_TOKEN,
+      { expiresIn: "1hr" }
+    );
+    res.cookie('token',token)
+    res.send("login");
+  } else {
+    res.status(401).send("Unauthoraized Access! Wrong Password");
+  }
+};
 
-module.exports = {login}
+module.exports = { login };
